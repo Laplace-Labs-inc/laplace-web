0d6b0eac50995dedf9bd26b8e0db18fefb6979fb
sukryu <hjinhyeog162@gmail.com>
2026-04-21 09:55:56 +0000

    fix: add base config vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // 👈 이거 추가
 
 export default defineConfig({
+  base: "/laplace-web/",
   plugins: [
     tailwindcss(), // 👈 이거 추가
     react()
